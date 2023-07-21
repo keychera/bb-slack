4f281d6b805575af0503a446e42dbf5de3761420
Kevin Erdiza Yogatama <kevin.yogatama@virgoku.id>
2023-07-21 15:56:19 +0700

    try github-actions

--- src/main.clj
+++ src/main.clj
@@ -4,8 +4,11 @@
 
 (defn -main [& args]
   (let [{:as opts
+         :keys [help]
          :slack/keys [text token channel-id]} (cli/parse-opts args)]
     (println opts)
-    (slack/chat #:slack{:token token
-                        :channel-id channel-id
-                        :text (or text "hello from bb-slack!")})))
+    (if help
+      (println "hello from bb-slack")
+      (slack/chat #:slack{:token token
+                          :channel-id channel-id
+                          :text (or text "hello from bb-slack!")}))))
